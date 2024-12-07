d9516e211aa23e03c2c97631c866f664a4ce19f1
fatmanurbursa <fatmanurbursaa@gmail.com>
2024-12-07 21:19:48 +0300

    rev

--- todolist.js
+++ todolist.js
@@ -4,8 +4,8 @@ $(document).ready(function() {
 
     
 $("#c").click(function(){
-    $("#li1").addClass("d");
-    console.log("tıklandı");
+    $("#li1").hide();
+    
 
 })
 
@@ -24,3 +24,41 @@ $("#r").click(function(){
     console.log("tıklandı");
 
 })});
+
+const ul=document.querySelector("#tasklist");
+const submit=document.querySelector("#btn");
+const input=document.querySelector(".input-group");
+
+const di=document.querySelector(".header");
+
+submit.addEventListener("click",addNewItem);
+
+function addNewItem(e){
+    if(input.value===""){
+   alert("add new item");
+    }else{
+        const li=document.createElement("li");
+        li.appendChild(document.createTextNode(input.value));
+        const i1=document.createElement("i");
+        i1.classList="fa-solid fa-xmark";
+        i1.id="r";
+    
+        const i2=document.createElement("i");
+        i2.classList="fa-solid fa-check";
+        i2.id="c";
+        
+        
+        li.appendChild(i1);
+        li.appendChild(i2);
+       
+        ul.appendChild(li);
+        
+        input.value="";
+        e.preventDefault();
+    }
+
+    
+
+
+}
+
