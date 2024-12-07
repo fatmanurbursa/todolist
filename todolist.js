45794e06ccf66a75fd517f7a8b41aa8dc2e9d96a
fatmanurbursa <fatmanurbursaa@gmail.com>
2024-12-07 16:14:11 +0300

    first commit

---
+++ todolist.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+
+
+
+    
+$("#c").click(function(){
+    $("#li1").addClass("d");
+    console.log("tıklandı");
+
+})
+
+
+
+
+
+
+
+});
+
+$(document).ready(function() {
+
+$("#r").click(function(){
+    $("#li1").addClass("j");
+    console.log("tıklandı");
+
+})});
