f3465c1545a636dff52dea584e2fbf903390da5e
fatmanurbursa <fatmanurbursaa@gmail.com>
2024-12-15 20:29:34 +0300

    rvz

--- todolist.js
+++ todolist.js
@@ -1,64 +1,100 @@
-$(document).ready(function() {
+
+const input=document.querySelector("#input");
+const button=document.querySelector("#btn");
+const c=document.querySelector("#c");
+const r=document.querySelector("#r");
+const ul=document.querySelector("#tasklist");
+
+
+const di=document.querySelector(".header");
+
+c.addEventListener("click",function(e){
+
+    
+    if(confirm("Are you sure you want to delete?")){
+       $("li").hide();
+
+    };
+
+});
+
+
+
+r.addEventListener("click",function(e){
+    document.querySelector("li").style.backgroundColor="red";
+});
+
+
 
 
 
     
-$("#c").click(function(){
-    $("#li1").hide();
+   
+    
+ 
+    
+   button.addEventListener("click",addNewItem);
+    
+    function addNewItem(e){
+        if(input.value===""){
+       alert("add new item");
+        }else{
+            const li=document.createElement("li");
+            li.appendChild(document.createTextNode(input.value));
+            const i1=document.createElement("i");
+            i1.classList="fa-solid fa-xmark";
+            i1.id="r";
+        
+            const i2=document.createElement("i");
+            i2.classList="fa-solid fa-check";
+            i2.id="c";
+            
+            
+            li.appendChild(i1);
+            li.appendChild(i2);
+           
+            ul.appendChild(li);
+    
+            ul.classList.add("list-group");
+            li.classList.add("list-group-item");
+            
+            input.value="";
+            e.preventDefault();
+        }
+    
+        
+    
+    
+    }
+
+
+
+
+
+
+    
+    
+    
+    
+    
+    
+    
     
 
-})
 
 
 
 
 
+document.querySelector(".container").addEventListener("mouseover",function(e) {
 
+    $(".bilgilendirme").removeClass("bilgilendirme").addClass("yenibilgilendirme");
 
 });
 
 $(document).ready(function() {
+    $("#iconbilgi").click(function(){
+        $(".yenibilgilendirme").hide(2000);
 
-$("#r").click(function(){
-    $("#li1").addClass("j");
-    console.log("tıklandı");
-
-})});
-
-const ul=document.querySelector("#tasklist");
-const submit=document.querySelector("#btn");
-const input=document.querySelector(".input-group");
-
-const di=document.querySelector(".header");
-
-submit.addEventListener("click",addNewItem);
-
-function addNewItem(e){
-    if(input.value===""){
-   alert("add new item");
-    }else{
-        const li=document.createElement("li");
-        li.appendChild(document.createTextNode(input.value));
-        const i1=document.createElement("i");
-        i1.classList="fa-solid fa-xmark";
-        i1.id="r";
-    
-        const i2=document.createElement("i");
-        i2.classList="fa-solid fa-check";
-        i2.id="c";
-        
-        
-        li.appendChild(i1);
-        li.appendChild(i2);
-       
-        ul.appendChild(li);
-        
-        input.value="";
-        e.preventDefault();
-    }
-
-    
-
-
-}
-
+    });
+});
